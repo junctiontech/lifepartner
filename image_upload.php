c2269840d6cbf37945c71b7c533a319f5a273822
Administrator <Administrator@j1>
2016-07-16 18:37:31 +0530

    g

--- image_upload.php
+++ image_upload.php
@@ -1,19 +1,23 @@
 <?php
 $image = $_POST['image'];
-print_r($image);die;
 
-$KEY_IMAGE = $_FILES['KEY_IMAGE']['name'];
-//print_r($data);
-$CONNECTION=mysqli_connect("localhost","root","initial1$","LifePartner");
-if(!$CONNECTION)
-{
-	echo "Database not found or There is an error in connecting to DB!! Please fix this!!!";
-	exit();
-}else{
-move_uploaded_file($_FILES['KEY_IMAGE']['tmp_name'],'image/'.$KEY_IMAGE['tmp_name']);
+$path = "images/$id.png";
+file_put_contents($path,base64_decode($image));
+echo "Successfully Uploaded";
+
+
+// $KEY_IMAGE = $_FILES['KEY_IMAGE']['name'];
+// //print_r($data);
+// $CONNECTION=mysqli_connect("localhost","root","initial1$","LifePartner");
+// if(!$CONNECTION)
+// {
+// 	echo "Database not found or There is an error in connecting to DB!! Please fix this!!!";
+// 	exit();
+// }else{
+// move_uploaded_file($_FILES['KEY_IMAGE']['tmp_name'],'image/'.$KEY_IMAGE['tmp_name']);
 		
-$sql="inser into tbl_name fields(image) values('$KEY_IMAGE')";
-mysqli_query($CONNECTION,$sql);
+// $sql="inser into tbl_name fields(image) values('$KEY_IMAGE')";
+// mysqli_query($CONNECTION,$sql);
 
 
-}
+//}
