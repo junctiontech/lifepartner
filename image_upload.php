3ca14e76bf4387776e7d010739bc05093ac9336e
Administrator <Administrator@j1>
2016-07-18 16:04:35 +0530

    rer

--- image_upload.php
+++ image_upload.php
@@ -37,10 +37,10 @@ if(!$CONNECTION)
 		
 		
 	
-	}else if(isset($_GET['action'])&& $_GET['action']=="download"){
-		$countrow=mysqli_query($CONNECTION,"select imageName from imagedata where s_no='10'");
-	echo $countrow;	$data1 = mysqli_fetch_array($countrow);print_r($data1);
-		echo $data1['imageName']; 
+	}else if(isset($_GET['action'])&& $_GET['action']=="download"){ echo 'hii';die;
+// 		$countrow=mysqli_query($CONNECTION,"select imageName from imagedata where s_no='10'");
+// 	echo $countrow;	$data1 = mysqli_fetch_array($countrow);print_r($data1);
+// 		echo $data1['imageName']; 
 	}
 }
 
