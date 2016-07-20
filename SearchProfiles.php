04ff64646702a67d993715e26e864e580175f18c
Administrator <Administrator@j1>
2016-07-20 13:38:19 +0530

    utyu

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -1,75 +1,75 @@
 <?php
 echo "hi";die;
-//print_r($data);
-$CONNECTION=mysqli_connect("localhost","root","initial1$","LifePartner");
-if(!$CONNECTION)
-{
-	echo "Database not found or There is an error in connecting to DB!! Please fix this!!!";
-	exit();
-}else{
-	echo "hi";die;
-	echo $_POST;die;
-	$min_age= $_POST['min_age'];
-	$max_age= $_POST['max_age'];
-	$bride_groom= $_POST['bride_groom'];
-	$gautr= $_POST['gautr'];
-// 	$gautr_nanihal= $_POST['gautr_nanihal'];
-	$manglik= $_POST['manglik'];
-	$income= $_POST['income'];
+// //print_r($data);
+// $CONNECTION=mysqli_connect("localhost","root","initial1$","LifePartner");
+// if(!$CONNECTION)
+// {
+// 	echo "Database not found or There is an error in connecting to DB!! Please fix this!!!";
+// 	exit();
+// }else{
+// 	echo "hi";die;
+// 	echo $_POST;die;
+// 	$min_age= $_POST['min_age'];
+// 	$max_age= $_POST['max_age'];
+// 	$bride_groom= $_POST['bride_groom'];
+// 	$gautr= $_POST['gautr'];
+// // 	$gautr_nanihal= $_POST['gautr_nanihal'];
+// 	$manglik= $_POST['manglik'];
+// 	$income= $_POST['income'];
 	
 	
 	
-	$querySearch="Select * from Profiles where 	gender='$bride_groom' AND manglik='$manglik' AND income='$income' AND gautr='$gautr'";
-	$query=mysqli_query($CONNECTION,$querySearch);
-$searchResult=array();
-	if($query){
+// 	$querySearch="Select * from Profiles where 	gender='$bride_groom' AND manglik='$manglik' AND income='$income' AND gautr='$gautr'";
+// 	$query=mysqli_query($CONNECTION,$querySearch);
+// $searchResult=array();
+// 	if($query){
 		
 	
-		while($result=mysqli_fetch_array($query)){
-			$from = new DateTime($result['dateOfBirth']);
-			$to   = new DateTime('today');
-			$age = $from->diff($to)->y;
+// 		while($result=mysqli_fetch_array($query)){
+// 			$from = new DateTime($result['dateOfBirth']);
+// 			$to   = new DateTime('today');
+// 			$age = $from->diff($to)->y;
 			
-			if (($min_age>=$age<=$max_age)){
-				$searchResult[]= array('firstName'=>$result['firstName'],
-						'lastName'=>$result['lastName'],
-						'fatherName'=>$result['fatherName'],
-						'dateOfBirth'=>$result['dateOfBirth'],
-						'birthPlace'=>$result['birthPlace'],
-						'heightOfUser'=>$result['heightOfUser'],
-						'birthTime'=>$result['birthTime'],
-						'highestQualification'=>$result['highestQualification'],
-						'TypeOfBusiness'=>$result['TypeOfBusiness'],
-						'business'=>$result['business'],
-						'income'=>$result['income'],
-						'TypeOfFatherBusiness'=>$result['TypeOfFatherBusiness'],
-						'fatherBusiness'=>$result['fatherBusiness'],
-						'fatherIncome'=>$result['fatherIncome'],
-						'gautr'=>$result['gautr'],
-						'gautrNanihal'=>$result['gautrNanihal'],
-						'zodiacSign'=>$result['zodiacSign'],
-						'star'=>$result['star'],
-						'saturn'=>$result['saturn'],
-						'manglik'=>$result['manglik'],
-						'currentAddress'=>$result['currentAddress'],
-						'permanentAddress'=>$result['permanentAddress'],
-						'emailId'=>$result['emailId'],
-						'mobileNumber'=>$result['mobileNumber'],
-						'WhatsAppNumber'=>$result['WhatsAppNumber'],
-						'imageName'=>"http://192.168.1.151/lifepartner/images/".$result['imageName']
+// 			if (($min_age>=$age<=$max_age)){
+// 				$searchResult[]= array('firstName'=>$result['firstName'],
+// 						'lastName'=>$result['lastName'],
+// 						'fatherName'=>$result['fatherName'],
+// 						'dateOfBirth'=>$result['dateOfBirth'],
+// 						'birthPlace'=>$result['birthPlace'],
+// 						'heightOfUser'=>$result['heightOfUser'],
+// 						'birthTime'=>$result['birthTime'],
+// 						'highestQualification'=>$result['highestQualification'],
+// 						'TypeOfBusiness'=>$result['TypeOfBusiness'],
+// 						'business'=>$result['business'],
+// 						'income'=>$result['income'],
+// 						'TypeOfFatherBusiness'=>$result['TypeOfFatherBusiness'],
+// 						'fatherBusiness'=>$result['fatherBusiness'],
+// 						'fatherIncome'=>$result['fatherIncome'],
+// 						'gautr'=>$result['gautr'],
+// 						'gautrNanihal'=>$result['gautrNanihal'],
+// 						'zodiacSign'=>$result['zodiacSign'],
+// 						'star'=>$result['star'],
+// 						'saturn'=>$result['saturn'],
+// 						'manglik'=>$result['manglik'],
+// 						'currentAddress'=>$result['currentAddress'],
+// 						'permanentAddress'=>$result['permanentAddress'],
+// 						'emailId'=>$result['emailId'],
+// 						'mobileNumber'=>$result['mobileNumber'],
+// 						'WhatsAppNumber'=>$result['WhatsAppNumber'],
+// 						'imageName'=>"http://192.168.1.151/lifepartner/images/".$result['imageName']
 							
 			
-				);
-			}
-		}
+// 				);
+// 			}
+// 		}
 		
 	
 		
-	}
+// 	}
 	
-	print_r(json_encode($searchResult));
+// 	print_r(json_encode($searchResult));
 
 	
 	
 	
-}
+// }
