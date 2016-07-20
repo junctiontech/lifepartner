0d29fea4935d3b3b3c342236b5c4b95d0cd2f79e
Administrator <Administrator@j1>
2016-07-20 13:39:20 +0530

    rtr

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -1,15 +1,15 @@
 <?php
-echo "hi";die;
-// //print_r($data);
-// $CONNECTION=mysqli_connect("localhost","root","initial1$","LifePartner");
-// if(!$CONNECTION)
-// {
-// 	echo "Database not found or There is an error in connecting to DB!! Please fix this!!!";
-// 	exit();
-// }else{
-// 	echo "hi";die;
-// 	echo $_POST;die;
-// 	$min_age= $_POST['min_age'];
+
+//print_r($data);
+$CONNECTION=mysqli_connect("localhost","root","initial1$","LifePartner");
+if(!$CONNECTION)
+{
+	echo "Database not found or There is an error in connecting to DB!! Please fix this!!!";
+	exit();
+}else{
+	echo "hi";die;
+	echo $_POST;die;
+	$min_age= $_POST['min_age'];
 // 	$max_age= $_POST['max_age'];
 // 	$bride_groom= $_POST['bride_groom'];
 // 	$gautr= $_POST['gautr'];
@@ -72,4 +72,4 @@ echo "hi";die;
 	
 	
 	
-// }
+}
