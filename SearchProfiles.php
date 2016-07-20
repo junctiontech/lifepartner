6d515ec7359fe165beff6aef614afc10bbb60e62
Administrator <Administrator@j1>
2016-07-20 13:08:29 +0530

    g

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -11,15 +11,60 @@ if(!$CONNECTION)
 	$max_age= $_POST['max_age'];
 	$bride_groom= $_POST['bride_groom'];
 	$gautr= $_POST['gautr'];
-	$gautr_nanihal= $_POST['gautr_nanihal'];
+// 	$gautr_nanihal= $_POST['gautr_nanihal'];
 	$manglik= $_POST['manglik'];
 	$income= $_POST['income'];
 	
-// 	$querySearch="Select * from Profiles where EmailID='$EmailID' or MobileNumber='$MobileNumber'";
-// 	$query=mysqli_query($CONNECTION,$querySearch);
-	$from = new DateTime('1993-07-31');
-	$to   = new DateTime('today');
-	echo $from->diff($to)->y;
+	$querySearch="Select * from Profiles where 	gender='$bride_groom' AND manglik='$manglik' AND income='$income' AND gautr='$gautr'";
+	$query=mysqli_query($CONNECTION,$querySearch);
+$searchResult=array();
+	if($query){
+		
+	
+		while($result=mysqli_fetch_array($query)){
+			$from = new DateTime($result['dateOfBirth']);
+			$to   = new DateTime('today');
+			$age = $from->diff($to)->y;
+			
+			if (($min_age>=$age<=$max_age)){
+				$searchResult[]= array('firstName'=>$result['firstName'],
+						'lastName'=>$result['lastName'],
+						'fatherName'=>$result['fatherName'],
+						'dateOfBirth'=>$result['dateOfBirth'],
+						'birthPlace'=>$result['birthPlace'],
+						'heightOfUser'=>$result['heightOfUser'],
+						'birthTime'=>$result['birthTime'],
+						'highestQualification'=>$result['highestQualification'],
+						'TypeOfBusiness'=>$result['TypeOfBusiness'],
+						'business'=>$result['business'],
+						'income'=>$result['income'],
+						'TypeOfFatherBusiness'=>$result['TypeOfFatherBusiness'],
+						'fatherBusiness'=>$result['fatherBusiness'],
+						'fatherIncome'=>$result['fatherIncome'],
+						'gautr'=>$result['gautr'],
+						'gautrNanihal'=>$result['gautrNanihal'],
+						'zodiacSign'=>$result['zodiacSign'],
+						'star'=>$result['star'],
+						'saturn'=>$result['saturn'],
+						'manglik'=>$result['manglik'],
+						'currentAddress'=>$result['currentAddress'],
+						'permanentAddress'=>$result['permanentAddress'],
+						'emailId'=>$result['emailId'],
+						'mobileNumber'=>$result['mobileNumber'],
+						'WhatsAppNumber'=>$result['WhatsAppNumber'],
+						'imageName'=>"http://192.168.1.151/lifepartner/images/".$result['imageName']
+							
+			
+				);
+			}
+		}
+		
+	
+		
+	}
+	
+	print_r(json_encode($searchResult));
+
 	
 	
 	
