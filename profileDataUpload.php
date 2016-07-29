08215519d5eac241e5df3a6fc1df22dd8b321d76
Administrator <Administrator@j1>
2016-07-29 12:24:08 +0530

    yrtyrt

--- profileDataUpload.php
+++ profileDataUpload.php
@@ -39,10 +39,10 @@ if(!$CONNECTION)
 		}else {	 
 			$querySearch="Select imageName from Profiles where no='$serverProfileId'";				
 		$ress=	mysqli_query($CONNECTION,$querySearch);
+			echo $querySearch;die;
 			
-			
-			 $imagename =mysqli_result_assoc($ress);print_r($imagename);die;
-			echo  $imagename['imageName'];die;
+			// $imagename =mysqli_result_assoc($ress);print_r($imagename);die;
+			//echo  $imagename['imageName'];die;
 			// echo $resultsearch;
 			// $imagename=$resultsearch['imageName'];
 			 
