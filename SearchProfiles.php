d5a8d93d6716fafec2a8248c1f6826b08db47773
Junction Ankit <Junction Ankit@JunctionAnkit>
2016-08-26 17:02:43 +0530

    ncjsd

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -23,7 +23,7 @@ if(!$CONNECTION)
 	if(!empty($caste && $caste!=='Select')){ $query.=" and caste='$caste'"; }
 	if(!empty($subCaste && $subCaste!=='Select')){ $query.=" and subcaste='$subCaste'"; }
 	if(!empty($minHeight) && $minHeight!=='Select'){ $query.=" and heightOfUser>='$minHeight' and heightOfUser<='$maxHeight'"; }
-	$querySearch="Select * from Profiles where $query LIMIT 100";
+	$querySearch="Select * from Profiles where $query LIMIT 100";echo $querySearch;die;
 	$query=mysqli_query($CONNECTION,$querySearch);
  	$searchResult=array();
  	if($query){
