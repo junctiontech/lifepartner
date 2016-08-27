3efb4be7b527da05fe35bbf1d7d3ec8306c3815e
Junction Ankit <Junction Ankit@JunctionAnkit>
2016-08-27 17:45:39 +0530

    sdklfnsd

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -18,23 +18,23 @@ if(!$CONNECTION)
 	$income_above_below=$_POST['income_above_below'];
 	$explode=explode(' ',$income);
 	$incomes=$explode[0].',00,000';
-	if(isset($income_above_below)&&!empty($income_above_below))
-	{
-		if(strcasecmp($income_above_below,'above')==0)  
-		{
-			$incomeIdentity='>';
-			if(!empty($incomes)){ $query.=" and income$incomeIdentity='$incomes' and income!='none'"; }
-		}
-		if(strcasecmp($income_above_below,'below')==0)  
-		{
-			$incomeIdentity='<';
-			if(!empty($incomes)){ $query.=" and income$incomeIdentity='$incomes' and income='none'"; }
-		}
-	}//echo $incomes; echo $incomeIdentity;die;
 	$caste=$_POST['caste'];
 	$subCaste=$_POST['subcaste'];
 	$registeredId = $_POST['registeredId'];
 	if(!empty($registeredId)){ $query=" registerUserID!='$registeredId'"; }
+	if(isset($income_above_below)&&!empty($income_above_below))
+	{
+		if(strcasecmp($income_above_below,'above')==0)
+		{
+			$incomeIdentity='>';
+			if(!empty($incomes)){ $query.=" and income$incomeIdentity='$incomes' and income!='none'"; }
+		}
+		if(strcasecmp($income_above_below,'below')==0)
+		{
+			$incomeIdentity='<';
+			if(!empty($incomes)){ $query.=" and income$incomeIdentity='$incomes' and income='none'"; }
+		}
+	}//echo $query;die;
 	if(!empty($bride_groom)){ $query.=" and gender='$bride_groom'"; }
 	if(!empty($city && $city!=='Select')){ $query.=" and city='$city'"; }
 	if(!empty($caste && $caste!=='Select')){ $query.=" and caste='$caste'"; }
