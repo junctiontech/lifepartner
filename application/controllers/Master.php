f1471e70a4bf431bd317898dbd5530bd33949e49
Junction Ankit <Junction Ankit@JunctionAnkit>
2016-08-27 18:58:13 +0530

    jdfds

--- application/controllers/Master.php
+++ application/controllers/Master.php
@@ -58,7 +58,7 @@
 			{
 				if(strcasecmp($incomeIdentity,'>')==0)
 				{
-					if(!empty($incomes) && $incomes!=='Select'){ $query.=" and income$incomeIdentity='$incomes' or income!='none'"; }
+					if(!empty($incomes) && $incomes!=='Select'){ $query.=" and income!='none' or income$incomeIdentity='$incomes' "; }
 				}
 				if(strcasecmp($incomeIdentity,'<')==0)
 				{
