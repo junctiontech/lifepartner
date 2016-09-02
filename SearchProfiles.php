e07e71ed6b466c2c6fadc87230c39eec9b4b4353
Junction Ankit <Junction Ankit@JunctionAnkit>
2016-09-02 13:26:32 +0530

    bjcbxfv

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -40,7 +40,7 @@ if(!$CONNECTION)
 	if(!empty($caste && $caste!=='Select')){ $query.=" and caste='$caste'"; }
 	if(!empty($subCaste && $subCaste!=='Select')){ $query.=" and subcaste='$subCaste'"; }//echo $query;
 	if(!empty($minHeight) && $minHeight!=='Select'){ $query.=" and heightOfUser>='$minHeight' and heightOfUser<='$maxHeight'"; }
-	$querySearch="Select * from Profiles where $query LIMIT 100";echo $querySearch;die;
+	$querySearch="Select * from Profiles where $query LIMIT 100";//echo $querySearch;die;
 	$query=mysqli_query($CONNECTION,$querySearch);
  	$searchResult=array();
  	if($query)
@@ -57,7 +57,7 @@ if(!$CONNECTION)
 			{ 
 				$from = new DateTime($result['dateOfBirth']);
 				$to   = new DateTime('today');
-				$age = $from->diff($to)->y;
+				$age = $from->diff($to)->y; echo $min_age;echo $age;echo $to;echo $max_age;die;
 				if ($min_age<=$age && $age <=$max_age)
 				{
 					$searchResult[]= array(
