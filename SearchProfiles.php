987b10c94d35fbc0131ae8a999b83f5a2a9df79e
Junction Ankit <Junction Ankit@JunctionAnkit>
2016-09-02 13:59:16 +0530

    jnkdjf

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -102,9 +102,9 @@ if(!$CONNECTION)
 				}
 			}
 		}
-	}echo count($searchResult);
+	}//echo count($searchResult);
 	//'imageName'=>"http://192.168.1.151/lifepartner/images/".$result['imageName']
-	//print_r(json_encode($searchResult));
+	print_r(json_encode($searchResult));
 
 	
 	
