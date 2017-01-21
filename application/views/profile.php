89a6f779a8fe90997f86c63ec8dffe8966a9f923
sarva <sarva@192.168.1.10>
2017-01-21 17:02:37 +0530

    changes

--- application/views/profile.php
+++ application/views/profile.php
@@ -63,13 +63,12 @@
 									
 									<?php } ?>
 									  <!--<div class="fileupload-preview fileupload-exists thumbnail" style="max-width: 120px; max-height: 120px; line-height: 20px;"></div>-->
-										<div>
-								</div></div>
+										</div>
 							</div>
 							<div class="row col-sm-4">
 								<div class="form-group">
-									<label class="col-sm-2 control-label" for="field-1">Gender<span style="color:red;"> *</span></label>
-									<div class="col-sm-10">
+									<label class="col-sm-4 control-label" for="field-1">Gender<span style="color:red;"> *</span></label>
+									<div class="col-sm-8">
 										<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->gender) &&$profile[0]->gender=='M'){ echo 'Male'; }else{ echo 'Fe Male';}?>" id="field-1" placeholder="Organization Name"  >
 									</div>
 								</div>
@@ -80,16 +79,16 @@
 						<div class="col-sm-12">
 							<div class="col-sm-6">
 								<div class=" form-group">
-									<label class="col-sm-2 control-label" for="field-1">First Name<span style="color:red;"> *</span></label>
-									<div class="col-sm-10">
+									<label class="col-sm-4 control-label" for="field-1">First Name<span style="color:red;"> *</span></label>
+									<div class="col-sm-8">
 										<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->firstName)){ echo $profile[0]->firstName; }?>" id="field-1" >
 									</div>
 								</div>
 								</div>
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Last Name<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Last Name<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->lastName)){ echo $profile[0]->lastName; }?>" id="field-1" placeholder="Organization Name"  >
 										</div>
 									</div>
@@ -100,16 +99,16 @@
 							<div class="col-sm-12">
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Date Of Birth<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Date Of Birth<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->dateOfBirth)){ echo date('d/m/y',strtotime($profile[0]->dateOfBirth)); }?>" id="field-1" >
 										</div>
 									</div>
 								</div>
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Highest Quallification<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Highest Quallification<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->highestQualification)){ echo $profile[0]->highestQualification; }?>" id="field-1" >
 										</div>
 									</div>
@@ -120,16 +119,16 @@
 							<div class="col-sm-12">
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Business<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Business<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->business)){ echo $profile[0]->business; }?>" id="field-1" >
 										</div>
 									</div>
 								</div>
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Income<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Income<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->income)){ echo $profile[0]->income; }?>" id="field-1" >
 										</div>
 									</div>
@@ -140,16 +139,16 @@
 							<div class="col-sm-12">
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1"> Currunt Address<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1"> Currunt Address<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<textarea class="form-control name="organization_name"><?php if(isset($profile[0]->currentAddress)){ echo $profile[0]->currentAddress; }?></textarea>
 										</div>
 									</div>
 								</div>
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Permanent Address<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Permanent Address<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<textarea class="form-control name="organization_name" ><?php if(isset($profile[0]->permanentAddress)){ echo $profile[0]->permanentAddress; }?></textarea>
 										</div>
 									</div>
@@ -160,16 +159,16 @@
 							<div class="col-sm-12">
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Mobile Number<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Mobile Number<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->mobileNumber)){ echo $profile[0]->mobileNumber; }?>" id="field-1" >
 										</div>
 									</div>
 								</div>
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Email<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Email<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->emailId)){ echo $profile[0]->emailId; }?>" id="field-1" >
 										</div>
 									</div>
@@ -180,16 +179,16 @@
 							<div class="col-sm-12">
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Father Name<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Father Name<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->fatherName)){ echo $profile[0]->fatherName; }?>" id="field-1" >
 										</div>
 									</div>
 								</div>
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Father Business<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Father Business<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->fatherJobProfile)){ echo $profile[0]->fatherJobProfile; }?>" id="field-1" >
 										</div>
 									</div>
@@ -200,16 +199,16 @@
 							<div class="col-sm-12">
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Father Income<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Father Income<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->fatherIncome)){ echo $profile[0]->fatherIncome; }?>" id="field-1" >
 										</div>
 									</div>
 								</div>
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Birth Place<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Birth Place<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->birthPlace)){ echo $profile[0]->birthPlace; }?>" id="field-1" >
 										</div>
 									</div>
@@ -220,16 +219,16 @@
 							<div class="col-sm-12">
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Nanihal Gautra<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Nanihal Gautra<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->gautrNanihal)){ echo $profile[0]->gautrNanihal; }?>" id="field-1" >
 										</div>
 									</div>
 								</div>
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Zodiac Sign<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Zodiac Sign<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->zodiacSign)){ echo $profile[0]->zodiacSign; }?>" id="field-1" >
 										</div>
 									</div>
@@ -240,16 +239,16 @@
 							<div class="col-sm-12">
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Manglik<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Manglik<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->manglik)){ echo $profile[0]->manglik; }?>" id="field-1" >
 										</div>
 									</div>
 								</div>
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Saturn<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Saturn<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->saturn)){ echo $profile[0]->saturn; }?>" id="field-1" >
 										</div>
 									</div>
@@ -260,16 +259,16 @@
 							<div class="col-sm-12">
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Caste<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Caste<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->caste)){ echo $profile[0]->caste; }?>" id="field-1" >
 										</div>
 									</div>
 								</div>
 								<div class="col-sm-6">
 									<div class=" form-group">
-										<label class="col-sm-2 control-label" for="field-1">Sub Caste<span style="color:red;"> *</span></label>
-										<div class="col-sm-10">
+										<label class="col-sm-4 control-label" for="field-1">Sub Caste<span style="color:red;"> *</span></label>
+										<div class="col-sm-8">
 											<input type="text" class="form-control"  name="organization_name" value="<?php if(isset($profile[0]->subcaste)){ echo $profile[0]->subcaste; }?>" id="field-1" >
 										</div>
 									</div>
