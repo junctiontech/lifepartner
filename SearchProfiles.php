e3f8e21137df1ccd888afd45226973cdd3fd725c
sarva <sarva@192.168.1.14>
2017-01-27 17:55:25 +0530

    qwertyu

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -33,7 +33,7 @@ if(!$CONNECTION)
 		if(strcasecmp($income_above_below,'below')==0)
 		{
 			$incomeIdentity='<';$none=none;
-			if(!empty($incomes) && $incomes!=='Select'){ $query.=" and income<=$incomes or income='$none' "; }
+			if(!empty($incomes) && $incomes!=='Select'){ $query.=" and income<=$incomes or income=$none "; }
 		}
 	}
 	if(isset($manglik) && !empty($manglik)){ $query.=" and manglik='$manglik'"; }
