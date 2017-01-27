f2b46ca7460b965c1501331da37e9a8e8bf11f83
sarva <sarva@192.168.1.14>
2017-01-27 18:09:08 +0530

    changes

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -22,7 +22,7 @@ if(!$CONNECTION)
 	$subCaste=$_POST['subcaste'];
 	$registeredId = $_POST['registeredId'];
 	if(!empty($registeredId)){ $query=" registerUserID!='$registeredId' and status='unblock' "; }
-	if(!empty($bride_groom)){ $query.=" and gender='$bride_groom'"; } echo $query;die;
+	if(!empty($bride_groom)){ $query.=" and gender='$bride_groom'"; } echo $query;//die;
 	if(isset($income_above_below)&&!empty($income_above_below))
 	{
 		if(strcasecmp($income_above_below,'above')==0)
@@ -41,7 +41,7 @@ if(!$CONNECTION)
 	if(!empty($caste && $caste!=='Select')){ $query.=" and caste='$caste'"; }
 	if(!empty($subCaste && $subCaste!=='Select')){ $query.=" and subcaste='$subCaste'"; }//echo $query;
 	if(!empty($minHeight) && $minHeight!=='Select'){ $query.=" and heightOfUser>='$minHeight' and heightOfUser<='$maxHeight'"; }
-	$querySearch="Select * from Profiles where $query LIMIT 100";//echo $querySearch;//die;
+	$querySearch="Select * from Profiles where $query LIMIT 100";echo $querySearch;//die;
 	$query=mysqli_query($CONNECTION,$querySearch);//print_r($query);//die;
  	$searchResult=array();
  	if(mysqli_num_rows($query)!=0)
