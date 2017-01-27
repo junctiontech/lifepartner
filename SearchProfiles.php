e02202beb0b2ad6d0438570d9ac3273401a457b3
sarva <sarva@192.168.1.14>
2017-01-27 12:15:11 +0530

    changes

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -9,22 +9,22 @@ if(!$CONNECTION)
 	exit();
 }else{
 	//error_reporting('0');
-	$abc = json_decode($_POST['data'],true);
-	$min_age= $abc['min_age']; echo $min_age;//die;
-	$max_age=$abc['max_age']; //echo $min_age; echo $max_age;die;
-	$bride_groom= $abc['bride_groom'];
-	$manglik=$abc['manglik'];
-	$city= $abc['city'];
-	$minHeight=$abc['min_height'];
-	$maxHeight=$abc['max_height'];
-	$income=$abc['income'];
-	$income_above_below=$abc['income_above_below'];//echo $income_above_below;die;
+	//$abc = json_decode($_POST['data'],true);
+	$min_age= $_POST['min_age']; echo $min_age;//die;
+	$max_age=$_POST['max_age']; //echo $min_age; echo $max_age;die;
+	$bride_groom= $_POST['bride_groom'];
+	$manglik=$_POST['manglik'];
+	$city= $_POST['city'];
+	$minHeight=$_POST['min_height'];
+	$maxHeight=$_POST['max_height'];
+	$income=$_POST['income'];
+	$income_above_below=$_POST['income_above_below'];//echo $income_above_below;die;
 	$explode=explode(' ',$income); //echo $explode;die;
 	$incomes=$explode[0].'00000';
 	$incomes = str_replace(',', '', $incomes);
-	$caste=$abc['caste'];
-	$subCaste=$abc['subcaste'];
-	$registeredId = $abc['registeredId'];//echo $registeredId;
+	$caste=$_POST['caste'];
+	$subCaste=$_POST['subcaste'];
+	$registeredId = $_POST['registeredId'];//echo $registeredId;
 	if(!empty($registeredId)){ $query=" registerUserID!='$registeredId'"; }
 	if(!empty($bride_groom)){ $query.=" and gender='$bride_groom'"; }
 	if(isset($income_above_below)&&!empty($income_above_below)) 
