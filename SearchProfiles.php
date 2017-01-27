9fb890c8cfc39ba5a0177479255299ef2bac03dd
sarva <sarva@192.168.1.14>
2017-01-27 12:06:27 +0530

    changes

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -43,7 +43,7 @@ if(!$CONNECTION)
 	if(isset($manglik) && !empty($manglik)){ $query.=" and manglik='$manglik'"; }
 	if(!empty($city && $city!=='Select')){ $query.=" and city='$city'"; }
 	if(!empty($caste && $caste!=='Select')){ $query.=" and caste='$caste'"; }
-	if(!empty($subCaste && $subCaste!=='Select')){ $query.=" and subcaste='$subCaste'"; }//echo $query;die;
+	if(!empty($subCaste && $subCaste!=='Select')){ $query.=" and subcaste='$subCaste'"; }
 	if(!empty($minHeight) && $minHeight!=='Select'){ $query.=" and heightOfUser>='$minHeight' and heightOfUser<='$maxHeight'"; }
 	$querySearch="Select * from Profiles where $query LIMIT 100";echo $querySearch;die;
 	$query=mysqli_query($CONNECTION,$querySearch);//print_r($query);die;
