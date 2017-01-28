cbcbe90c3280d63ebb005a2c4925a90728f5d9a5
sarva <sarva@192.168.1.14>
2017-01-28 16:56:56 +0530

    chnages

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -41,7 +41,7 @@ if(!$CONNECTION)
 	if(!empty($caste && $caste!=='Select')){ $query.=" and caste='$caste'"; }
 	if(!empty($subCaste && $subCaste!=='Select')){ $query.=" and subcaste='$subCaste'"; }//echo $query;
 	if(!empty($minHeight) && $minHeight!=='Select'){ $query.=" and heightOfUser>='$minHeight' and heightOfUser<='$maxHeight'"; }
-	$querySearch="Select * from Profiles where $query LIMIT 100";//echo $querySearch;die;
+	$querySearch="Select * from Profiles where $query LIMIT 100";
 	$query=mysqli_query($CONNECTION,$querySearch);
  	$searchResult=array();
  	if(mysqli_num_rows($query)!=0)
