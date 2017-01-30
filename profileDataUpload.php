bbc750f27824d912c05b790aef97ac87d1da678b
sarva <sarva@192.168.1.14>
2017-01-30 13:13:35 +0530

    qwert

--- profileDataUpload.php
+++ profileDataUpload.php
@@ -10,9 +10,9 @@ if(!$CONNECTION)
 		$Totaldata=json_decode($_POST['json'],true);//print_r($Totaldata);//die;
 		$registerID= $_POST['registeredId'];
 		$resultUpload=array();
-		$salary = $Totaldata['income'];echo $salary;die;
-		$income = $Totaldata['profileData'][0]['income'];echo $income;
-		$incomes = str_replace(',', '',$income); echo $incomes ;die;
+		$salary = $Totaldata[0]['income'];echo $salary;die;
+		$income = $Totaldata['profileData'][0]['income'];
+		$incomes = str_replace(',', '',$income); 
 		$ProfileData = $Totaldata['profileData'];//print_r($ProfileData);die;
 		foreach ($ProfileData as $data){	
 		$id=substr(md5(microtime()),rand(0,26),5);
