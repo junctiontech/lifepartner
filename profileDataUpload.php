099d843f1b356c0e1b49adf2925a762b51a061a9
sarva <sarva@192.168.1.14>
2017-01-30 12:54:44 +0530

    qwer

--- profileDataUpload.php
+++ profileDataUpload.php
@@ -12,7 +12,7 @@ if(!$CONNECTION)
 		$resultUpload=array();
 		$income = $Totaldata['profileData'][0]['income'];
 		$income = str_replace(',', '',$income);
-		$ProfileData = $Totaldata['profileData'];
+		$ProfileData = $Totaldata['profileData'];print_r($ProfileData);die;
 		foreach ($ProfileData as $data){	
 		$id=substr(md5(microtime()),rand(0,26),5);
 		$name = $id.'_'.$registerID.'_'.date('dmy_H:i:s').'.jpeg';
