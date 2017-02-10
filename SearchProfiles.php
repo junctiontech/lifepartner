dee6b86e78ce107fead58d574a833dd77cf6f22e
sarva <sarva@192.168.1.9>
2017-02-10 15:43:03 +0530

    chnages

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -46,8 +46,8 @@ if(!$CONNECTION)
  	$searchResult=array();
  	if(mysqli_num_rows($query)!=0)
  	 {   
-		while($result=mysqli_num_rows($query))
-		 {  print_r($result['no']);
+		while($result=mysqli_fetch_row($query))
+		 {   print_r($result['no']);
 		 	$queryRequestContact="select * from requestContact where profileID='".$result['no']."'";
 			//echo $queryRequestContact;
 			$sql=mysqli_query($CONNECTION,$queryRequestContact);
