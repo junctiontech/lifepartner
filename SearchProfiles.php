7db040c993dbc077375fb738e8be375919889726
sarva <sarva@192.168.1.9>
2017-02-10 11:41:59 +0530

    changes

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -100,8 +100,7 @@ if(!$CONNECTION)
 							'city'=>$result['city'],
 							'caste'=>$result['caste'],
 							'subcaste'=>$result['subcaste'],
-							//'status'=>$result['status'],
-					     );
+							);
 				  }
 			}
 		}
