6203e3805a9b8b0146fbd037d6fbd2c9e2ed0fd2
sarva <sarva@LAPTOP-Q31P2N9C>
2017-02-11 01:27:54 +0530

    qwerty

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -105,7 +105,8 @@ if(!$CONNECTION)
 				  }
 			} 
 		}
-	}echo count($searchResult);
+	}echo "kdjfkj";
+	//echo count($searchResult);
 	//'imageName'=>"http://192.168.1.151/lifepartner/images/".$result['imageName']
 	print_r(json_encode($searchResult));
 
