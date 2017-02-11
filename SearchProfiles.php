5bb22c650893e839f148bfce81b7bcec29e34076
sarva <sarva@192.168.1.9>
2017-02-11 13:19:14 +0530

    changes

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -22,7 +22,7 @@ if(!$CONNECTION)
 	$subCaste=$_POST['subcaste'];
 	$registeredId = $_POST['registeredId'];//echo $registeredId;die;
 	if(!empty($registeredId)){ $query=" registerUserID!='$registeredId' and status='unblock' "; }
-	if(!empty($bride_groom)){ $query.=" and gender='$bride_groom'"; } 
+	if(!empty($bride_groom)){ $query .=" and gender='$bride_groom'"; } 
 	if(isset($income_above_below)&&!empty($income_above_below))
 	{
 		if(strcasecmp($income_above_below,'above')==0)
@@ -33,7 +33,7 @@ if(!$CONNECTION)
 		if(strcasecmp($income_above_below,'below')==0)
 		{
 			$incomeIdentity='<'; $none='none';
-			if(!empty($incomes) && $incomes!=='Select'){ $query .=" and income<=$incomes OR income=$none "; }
+			if(!empty($incomes) && $incomes!=='Select'){ $query .=" and income<=$incomes OR income='$none' "; }
 		}
 	}
 	if(isset($manglik) && !empty($manglik)){ $query .=" and manglik='$manglik'"; }
