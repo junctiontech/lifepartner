1908515ec482ad78aa080de177264fc9d396c8ca
sarva <sarva@192.168.1.9>
2017-02-11 12:36:26 +0530

    changes

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -22,7 +22,7 @@ if(!$CONNECTION)
 	$subCaste=$_POST['subcaste'];
 	$registeredId = $_POST['registeredId'];//echo $registeredId;die;
 	if(!empty($registeredId)){ $query=" registerUserID!='$registeredId' and status='unblock' "; }
-	if(!empty($bride_groom)){ $query .=" and gender='$bride_groom'"; } 
+	if(!empty($bride_groom)){ $query.=" and gender='$bride_groom'"; } 
 	if(isset($income_above_below)&&!empty($income_above_below))
 	{
 		if(strcasecmp($income_above_below,'above')==0)
