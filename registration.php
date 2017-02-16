abe2c745f4b8542f2c521430701f3be0446cb81f
sarva <sarva@192.168.1.3>
2017-02-16 15:58:48 +0530

    changes

--- registration.php
+++ registration.php
@@ -15,14 +15,12 @@ if(!$CONNECTION)
 		$userName = $data['userName'];
 		$EmailID = $data['EmailID'];
 		$MobileNumber = $data['MobileNumber'];
-		if(isset($MobileNumber)&& !empty($MobileNumber)&&count($MobileNumber)<10)
+		if(isset($MobileNumber)&& !empty($MobileNumber)&&count($MobileNumber)< 10)
 		{
 			$result= array('code'=>'200','message'=>'Mobile number not correct');
 			print_r(json_encode($result));die;
 		}
-		else{
-			$querySearch="Select * from RegisteredUser,Profiles where RegisteredUser.MobileNumber='$MobileNumber' and RegisteredUser.registerUserID=Profiles.registerUserID";//echo $querySearch;
-			}
+		$querySearch="Select * from RegisteredUser,Profiles where RegisteredUser.MobileNumber='$MobileNumber' and RegisteredUser.registerUserID=Profiles.registerUserID";//echo $querySearch;
 		$query=mysqli_query($CONNECTION,$querySearch);
 		$count=mysqli_num_rows($query);
 		while($result=mysqli_fetch_assoc($query))
