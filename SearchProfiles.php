52456ffcfcf421eacbe4a55ba67034d8b5268c9d
deep <deep.k.sharma@gmail.com>
2017-03-08 18:40:39 +0530

    qweryu

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -41,7 +41,7 @@ if(!$CONNECTION)
 		if(strcasecmp($income_above_below,'below')==0)
 		{
 			$incomeIdentity='<'; $none='none';
-			if(!empty($incomes) && $incomes!=='Select'){ $query .=" and income<=$incomes or income='$none' "; }
+			if(!empty($incomes) && $incomes!=='Select'){ $query .=" and income<=$incomes and income='$none' "; }
 		}
 	}
 	if(isset($manglik) && !empty($manglik))
