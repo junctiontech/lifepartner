c368c1aaf515c85a6e90acf929389a24862ecc43
deep <deep.k.sharma@gmail.com>
2017-03-11 17:15:43 +0530

    changes in searchprofile

--- SearchProfiles.php
+++ SearchProfiles.php
@@ -90,7 +90,6 @@ if(!$CONNECTION)
 				$sql=mysqli_query($CONNECTION,$queryRequestContact);
 				if(mysqli_num_rows($sql))
 				{
-					
 					//print_r($result['city']);
 					//print_r("entry in requestcontact");
 				}
